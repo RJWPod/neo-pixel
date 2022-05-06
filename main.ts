105a3a64098be6678fa1c0ed88ad0949c48a8b97
RJWPod <101339801+RJWPod@users.noreply.github.com>
2022-05-06 09:30:09 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,30 @@
+function Pedestrian_Crossing () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(1000)
+    for (let index = 0; index < 20; index++) {
+        basic.showString("" + (Pedestrian_Crossing_Time))
+        Pedestrian_Crossing_Time += -1
+    }
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+}
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 7) {
+        basic.pause(5000)
+        Car()
+    }
+})
 input.onButtonPressed(Button.A, function () {
     basic.pause(5000)
     Pedestrian_Crossing()
@@ -17,50 +44,6 @@ function RED () {
     range = Strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
-function Pedestrian_Crossing_VI () {
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . # . # .
-        # . . . #
-        `)
-    basic.pause(5000)
-    for (let index = 0; index < 20; index++) {
-        music.playTone(988, music.beat(BeatFraction.Half))
-        music.playTone(784, music.beat(BeatFraction.Half))
-        basic.showString("" + (Pedestrian_Crossing_Time))
-        Pedestrian_Crossing_Time += -1
-    }
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
-}
-function Pedestrian_Crossing () {
-    basic.showLeds(`
-        . . # . .
-        . # # # .
-        # . # . #
-        . # . # .
-        # . . . #
-        `)
-    basic.pause(5000)
-    for (let index = 0; index < 20; index++) {
-        basic.showString("" + (Pedestrian_Crossing_Time))
-        Pedestrian_Crossing_Time += -1
-    }
-    basic.showLeds(`
-        # . . . #
-        . # . # .
-        . . # . .
-        . # . # .
-        # . . . #
-        `)
-}
 function GREEN () {
     range = Strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -71,9 +54,9 @@ function GREEN () {
 }
 input.onButtonPressed(Button.B, function () {
     basic.pause(5000)
-    music.playTone(587, music.beat(BeatFraction.Half))
+    music.playTone(587, music.beat(BeatFraction.Quarter))
     music.playTone(698, music.beat(BeatFraction.Half))
-    Pedestrian_Crossing()
+    Pedestrian_Crossing_VI()
     basic.pause(3000)
     GREEN()
     basic.pause(20000)
@@ -82,6 +65,38 @@ input.onButtonPressed(Button.B, function () {
     RED()
     basic.pause(40000)
 })
+function Car () {
+    basic.showIcon(IconNames.No)
+    GREEN()
+    basic.pause(20000)
+    YELLOW()
+    basic.pause(4000)
+    RED()
+}
+function Pedestrian_Crossing_VI () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        # . . . #
+        `)
+    basic.pause(1000)
+    for (let index = 0; index < 20; index++) {
+        music.playTone(988, music.beat(BeatFraction.Half))
+        music.playTone(784, music.beat(BeatFraction.Half))
+        basic.showString("" + (Pedestrian_Crossing_Time))
+        Pedestrian_Crossing_Time += -1
+    }
+    music.playTone(698, music.beat(BeatFraction.Whole))
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+}
 function YELLOW () {
     range = Strip.range(0, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -90,11 +105,24 @@ function YELLOW () {
     range = Strip.range(2, 1)
     range.showColor(neopixel.colors(NeoPixelColors.Black))
 }
+let Distance = 0
 let range: neopixel.Strip = null
 let Strip: neopixel.Strip = null
 let Pedestrian_Crossing_Time = 0
 Pedestrian_Crossing_Time = 20
 Strip = neopixel.create(DigitalPin.P0, 3, NeoPixelMode.RGB)
 Strip.setBrightness(20)
+radio.setGroup(177)
 basic.showIcon(IconNames.No)
 RED()
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P1, 0)
+    control.waitMicros(2)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    control.waitMicros(10)
+    pins.digitalWritePin(DigitalPin.P1, 1)
+    Distance = pins.pulseIn(DigitalPin.P2, PulseValue.High) / 58
+    if (Distance == 5) {
+        radio.sendNumber(7)
+    }
+})
